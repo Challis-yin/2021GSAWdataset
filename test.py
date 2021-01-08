c720f0848966d35be38513ae6f5959ea172bea53
ycl <sdyinruichao@163.com>
2021-01-08 16:11:19 +0800

    ycl_first_change

---
+++ test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from flask import Flask ,jsonify,render_template,url_for,redirect,abort, request
+from flask_sqlalchemy import *
+
+app = Flask('kkk')
+@app.route('/', methods=['GET', 'POST'])
+def test1():
+    return render_template("index.html")
+
+@app.route('/search', methods=['GET', 'POST'])
+def test2():
+    return render_template("search.html")
+
+@app.route('/summary', methods=['GET', 'POST'])
+def test3():
+    return render_template("summary.html")
+
+app.run()
+
